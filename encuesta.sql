6365e7e5854886e066507098b380a3f31a866353
Psykotropiko <43467641+Psykotropiko@users.noreply.github.com>
2020-01-07 21:28:59 +0100

    Añado datos a la db.
    Autor: Manuel.
    Añado datos a usuarios, preguntas, encuesta.
    usuarios: id unico, correo + clave y status(1= usuario//2=adm)
    preguntas: 3-4 preguntas referentes a 1 tema de encuesta.
    encuesta: temas a tratar: salud,balonmano,informatica y diseño. Cada tema esta relacionado con un administrador de la aplicacion web.

--- encuesta.sql
+++ encuesta.sql
@@ -5,6 +5,7 @@ use encuestas;
 create table usuarios(
 id int PRIMARY KEY auto_increment not null,
 correo varchar(75),
+password varchar(50),
 status char(10) default 1
 ) Engine = InnoDB;
 
@@ -12,14 +13,15 @@ create table encuesta(
 id int PRIMARY KEY auto_increment not null,
 id_admin int,
 nombre varchar(100),
+descripcion varchar(200),
 FOREIGN KEY(id_admin) references usuarios(id)
 ON UPDATE CASCADE ON DELETE CASCADE
 ) Engine = InnoDB;
 
 create table pregunta(
 id int PRIMARY KEY auto_increment not null,
-texto_pregunta varchar(200),
-tipo_pregunta varchar(5)
+titulo_pregunta varchar(200),
+es_multiple boolean
 ) Engine = InnoDB;
 
 create table r_encuesta_pregunta(
@@ -57,3 +59,33 @@ FOREIGN KEY (id_pregunta) references pregunta(id),
 FOREIGN KEY (id_respuesta) references respuesta(id)
 ON UPDATE CASCADE ON DELETE CASCADE
 ) Engine = InnoDB;
+
+insert into usuarios(correo,password,status) values ("manuel@dev.es",1234,2);
+insert into usuarios(correo,password,status) values ("javi@dev.es",1234,2);
+insert into usuarios(correo,password,status) values ("luis@dev.es",1234,2);
+insert into usuarios(correo,password,status) values ("rene@dev.es",1234,2);
+
+insert into encuesta(id_admin,nombre,descripcion) values (1,"informatica","Realiza una encuesta sobre los distintos campos de éste tema.");
+insert into encuesta(id_admin,nombre,descripcion) values (2,"balonmano","Realiza una encuesta a cerca de éste deporte popular.");
+insert into encuesta(id_admin,nombre,descripcion) values (3,"salud","Realiza una encuesta detallada sobre un tema tan importante como es la salud.");
+insert into encuesta(id_admin,nombre,descripcion) values (4,"diseño","Realiza una encuesta para facilitarnos tu conocimiento a cerca del diseño web.");
+
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Utilizas mas Linux o Windows?",true);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Te gusta programar?",false);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Has roto algun ordenador por trastearlo demas?",false);
+
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Cuantos años llevas jugando?",true);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Has tenido alguna lesion?",true);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Te parece un deporte minoritario?", true);
+
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Fumas?",false);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Con que frecuencia?",true);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿A que edad empezaste?",true);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Piensas dejarlo este año?",false);
+
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Prefieres GIMP o Photoshop?",true);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Utilizas paint?",false);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Usas Photoshop para crear animaciones?",false);
+insert into pregunta(titulo_pregunta,es_multiple) values ("¿Eres usuario de boostrap?",false);
+
+
